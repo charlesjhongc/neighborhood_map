d891c367eab480755e66d5c1bd6b491c8d283f28
Charles Jhong <shad0356jc@gmail.com>
2018-01-02 20:35:30 +0800

    Basic infowindow

--- js/app.js
+++ js/app.js
@@ -1,43 +1,61 @@
-var map;
-var marker_data = [
-  {
-    lat: 25.038525,
-    lng: 121.498121,
-    title: 'Stella'
-  },
-  {
-    lat: 25.0304996,
-    lng: 121.5212562,
-    title: '老王'
-  }
-];
-
-function initMap() {
- map = new google.maps.Map(document.getElementById('map'),{
-   center: {lat:25.034597, lng:121.5126302},
-   zoom: 16
- });
- marker_data.forEach(function(element){
-   new google.maps.Marker({
-     position: {lat:element.lat, lng:element.lng},
-     map: map,
-     title: element.title
-   });
- });
+function Infomarker() {
 }
 
-function model(){
-
+function Model() {
+  this.locations = [
+    {
+      lat: 25.038525,
+      lng: 121.498121,
+      title: '今風'
+    },
+    {
+      lat: 25.0304996,
+      lng: 121.5212562,
+      title: '老王'
+    }
+  ];
 }
 
 function view(){
 
 }
 
-function Viewmodel(){
-  this.markers = marker_data;// use observable array??
-  this.setactive_marker = function(){
+function Viewmodel() {
+  var self = this;
+  this.infomarkers =  ko.observableArray();
+
+  this.initMap = function() {
+    self.map = new google.maps.Map(document.getElementById('map'),{
+      center: {lat:25.034597, lng:121.5126302},
+      zoom: 16
+    });
+    var bounds = new google.maps.LatLngBounds();
+    model.locations.forEach(function(element){
+      var new_infomarker = new Infomarker();
+      new_infomarker.gmarker = new google.maps.Marker({
+        position: {lat:element.lat, lng:element.lng},
+        map: self.map,
+        animation: google.maps.Animation.DROP,
+        title: element.title
+      });
+      new_infomarker.infowindow = new google.maps.InfoWindow({
+        content: element.title
+      });
+      new_infomarker.gmarker.addListener('click', function() {
+        new_infomarker.infowindow.open(self.map, new_infomarker.gmarker);//????????
+      });
+      bounds.extend(new_infomarker.gmarker.position);
+      self.infomarkers.push(new_infomarker);
+    });
+    self.map.fitBounds(bounds);
+  };
+
+  this.show_clicked_marker = function(clicked_marker) {
+    clicked_marker.infowindow.open(self.map, clicked_marker.gmarker);
+    self.map.setCenter(clicked_marker.gmarker.position);
   };
 }
 
-ko.applyBindings(new Viewmodel());
+var model = new Model();
+var viewmodel =  new Viewmodel();
+ko.applyBindings(viewmodel);
